9666d8411d7ece5c815511e143f799836c6e0243
maxinming <1993945031qq@.com>
2017-12-12 15:26:31 +0800

    123

---
+++ index.js
@@ -0,0 +1,42 @@
+/**
+ * Created by lenovo on 17/12/8 008.
+ */
+<!-- Initialize Swiper -->
+
+var swiper = new Swiper('.swiper-container', {
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    on:{
+        init: function(){
+            swiperAnimateCache(this); //隐藏动画元素
+            swiperAnimate(this); //初始化完成开始动画
+        },
+        slideChangeTransitionEnd: function(){
+            swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
+        }
+    }
+});
+
+
+//点击开始
+var music=document.getElementById("music");
+var musicsd=document.querySelectorAll(".musicsd")[0];
+var musicas=document.querySelectorAll(".musicas")[0];
+var audio=document.querySelectorAll("audio")[0];
+var as=1;
+music.onclick=function(){
+    if(as==1){
+        musicas.style.display="none";
+        musicsd.style.animation="none";
+        audio.pause();
+        as=0;
+    }else{
+        musicsd.style.display="block";
+        musicas.style.animation="circle 1s infinite linear";
+        audio.play();
+        as=1;
+    }
+};
+
